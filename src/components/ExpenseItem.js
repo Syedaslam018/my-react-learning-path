7934e6c4dbd7630f18114a825b18b5dc159b486e
Syed Aslam <syedaslam.me@gmail.com>
2023-09-09 19:21:58 +0530

    first commit of the react project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items</h2>
+      <ul>
+        <li>Food 200</li>
+        <li>cake 200</li>
+        <li>fuel 200</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ExpenseItem;
